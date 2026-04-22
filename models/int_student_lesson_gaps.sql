726538fd12859b51d341dc313c8abc69cc0898fb
alex.whatmore@gmail.com <alex.whatmore@gmail.com>
2026-04-22 14:34:03 +0000

    Final dbt project for churn and reactivation analysis (Feb 2026)

--- models/int_student_lesson_gaps.sql
+++ models/int_student_lesson_gaps.sql
@@ -1,19 +1,15 @@
-select
-    student_id,
-    completed_date,
-
-    lag(completed_date) over (
-        partition by student_id
-        order by completed_date
-    ) as previous_date,
-
-    date_diff(
+with base as (
+    select
+        student_id,
         completed_date,
         lag(completed_date) over (
             partition by student_id
             order by completed_date
-        ),
-        day
-    ) as days_since_last_lesson
+        ) as previous_date
+    from {{ ref('stg_lessons') }}
+)
 
-from {{ ref('stg_lessons') }}
+select
+    *,
+    date_diff(completed_date, previous_date, day) as days_since_last_lesson
+from base
